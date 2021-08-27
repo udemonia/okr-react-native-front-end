b30de9d7c6d7c21826dab8e17b7593abf461205f
udemonia <brandon.l.lambert@icloud.com>
2021-08-26 19:47:12 -0500

    working api auth

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,19 +1,6 @@
-// import React from 'react';
-// import {View, Text, StyleSheet, Button } from 'react-native';
-// import signUpScreen from './signUpScreen'
-
-// const loginInScreen = ({ navigation }) => {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Login Screen</Text>
-//         <Button title="need an account? sign up!" onPress={()=> navigation.navigate('SignUp')} />
-//         <Button title='submit' onPress={() => navigation.push('drawer')} />
-//       </View>
-//     )
-//   }
-
 import React, {useState, createRef} from 'react';
-import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 import {
   StyleSheet,
@@ -27,180 +14,186 @@ import {
   KeyboardAvoidingView,
   SafeAreaView
 } from 'react-native';
+const STORAGE_KEY = '@save_token'
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Loader from '../components/Loader';
+
+const storeDataString = async (value) => {
+  try {
+    await AsyncStorage.setItem(STORAGE_KEY, value)
+    // alert('Data Saved!')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//* We need to call login + getCurrentUser
   
   const LoginScreen = ({navigation}) => {
   
       //* State Management
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [errortext, setErrortext] = useState('');
-  
+    const [ userEmail, setUserEmail ] = useState('');
+    const [ userPassword, setUserPassword ] = useState('');
+    const [ loading, setLoading ] = useState(false);
+    const [ errortext, setErrortext ] = useState('');
+    const [ apiData, setApiData ] = useState([])
+    const [ token, setToken ] = useState('')
     const passwordInputRef = createRef();
-
   
     const handleSubmitPress = () => {
-      //* Lets handle missing info -
+        //* Lets handle missing info -
   
-      setErrortext('');
-      if (!userEmail) {
-        alert('Please fill Email');
-        return;
-      }
-      if (!userPassword) {
-        alert('Please fill Password');
-        return;
-      }
-  
-      //* If data, lets set loading to true - using the load component
-      setLoading(true);
-  
-      //* data to send.... 
-      let dataToSend = {email: userEmail, password: userPassword};
-      console.log(dataToSend)
-  
-      let formBody = [];
-      for (let key in dataToSend) {
-        let encodedKey = encodeURIComponent(key);
-        let encodedValue = encodeURIComponent(dataToSend[key]);
-        formBody.push(encodedKey + '=' + encodedValue);
-      }
-  
-      formBody = formBody.join('&');
-  
-      fetch('http://localhost:2002/api/v1/auth/login', {
-        method: 'POST',
-        body: {
-            email: userEmail,
-            password: userPassword
-          },
-        headers: {
-          //Header Defination
-          'Content-Type':
-          'application/x-www-form-urlencoded;charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((responseJson) => {
-          
-          //Hide Loader
-          setLoading(false);
-          // If server response message same as Data Matched
-          if (responseJson.status === 'success') {
-            AsyncStorage.setItem('user_id', responseJson.data.email);
-            console.log(responseJson.data.email);
-            navigation.replace('DrawerNavigationRoutes');
-          } else {
-            setErrortext(responseJson.msg);
-            console.log('Please check your email id or password');
-          }
-        })
-  
-        .catch((error) => {
-          //Hide Loader
-          setLoading(false);
-          console.error(error);
-        });
-    };
-  
-  
-  
-  
-    return (
+        setErrortext('');
+        if (!userEmail) {
+          alert('Please fill Email');
+          return;
+        }
+        if (!userPassword) {
+          alert('Please fill Password');
+          return;
+        }
       
-  
-      <View style={styles.mainBody}>
-        <SafeAreaView>
+        // //* If data, lets set loading to true - using the load component
+        // setLoading(true);
+      
+        //* data to send.... 
+        let dataToSend = {email: userEmail, password: userPassword};
+        console.log(dataToSend)
 
-        <Loader loading={loading} />
-        
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          // contentContainerStyle={{
-          //   flex: 1,
-          //   justifyContent: 'center',
-          //   alignContent: 'center',
-          // }}
-          >
-          <View>
-            <KeyboardAvoidingView enabled>
-              <View style={{flex: 1, alignItems: 'center'}}>
-                <Image
-                  source={require('../assets/rocket.png')}
-                  style={{
-                    width: '75%%',
-                    height: 200,
-                    resizeMode: 'center',
-                    margin: 30,
-                    borderRadius: 15
-                  }}
-                />
-              </View>
-              <View style={styles.SectionStyle}>
-                <TextInput
-                  style={styles.inputStyle}
-                  onChangeText={(UserEmail) =>
-                    setUserEmail(UserEmail)
+
+        }
+        const authPostRequest = async () => {
+          try {
+            let response = await fetch('http://192.168.1.231:2002/api/v1/auth/login', {
+              method: 'POST',
+              headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                "email": "1@1.com",
+                "password": '1234567'
+                // "email": userEmail,
+                // "password": userPassword
+              })
+              })
+              if (response.status === 200) {
+                let responseData = await response.json();
+                let JWTtoken = JSON.stringify(responseData.token).slice(1, -1);
+                const savedToken = await storeDataString(JWTtoken)
+                alert(JWTtoken)
+                try {
+                  let currentUserResponse = await fetch('http://192.168.1.231:2002/api/v1/auth/CurrentLoggedUser', {
+                    method: 'GET',
+                    headers: {
+                      "Accept": 'application/json',
+                      "Authorization": `Bearer ${JWTtoken}`
+                    }
+                  })
+                  let userData = await currentUserResponse.json();
+                  alert(JSON.stringify(userData.data, null, 2))
+                  if (currentUserResponse.status === 200) {
+
+                    navigation.navigate('drawer', {userData: userData})
+                    console.log(navigation)
+                    navigate               
                   }
-                  placeholder="email address"
-                  placeholderTextColor="#8b9cb5"
-                  autoCapitalize="none"
-                  keyboardType="email-address"
-                  returnKeyType="next"
-                  onSubmitEditing={() =>
-                    passwordInputRef.current &&
-                    passwordInputRef.current.focus()
-                  }
-                  underlineColorAndroid="#f000"
-                  blurOnSubmit={true}
-                />
+                  
+                } catch (error) {
+                  console.log(error)
+                }
+
+
+
+              } else {
+                alert('error' + response.status)
+              }
+          } catch (error) {
+            console.log(error)
+          }
+        }
+
+        return (
+          <View style={styles.mainBody}>
+            <SafeAreaView>
+            <ScrollView
+              keyboardShouldPersistTaps="handled"
+              >
+              <View>
+                <KeyboardAvoidingView enabled>
+                  <View style={{flex: 1, alignItems: 'center'}}>
+                    <Image
+                      source={require('../assets/rocket.png')}
+                      style={{
+                        width: '75%%',
+                        height: 200,
+                        resizeMode: 'center',
+                        margin: 30,
+                        borderRadius: 15
+                      }}
+                    />
+                  </View>
+                  <View style={styles.SectionStyle}>
+                    <TextInput
+                      style={styles.inputStyle}
+                      onChangeText={(UserEmail) =>
+                        setUserEmail(UserEmail)
+                      }
+                      placeholder="email address"
+                      placeholderTextColor="#8b9cb5"
+                      autoCapitalize="none"
+                      keyboardType="email-address"
+                      returnKeyType="next"
+                      onSubmitEditing={() =>
+                        passwordInputRef.current &&
+                        passwordInputRef.current.focus()
+                      }
+                      underlineColorAndroid="#f000"
+                      blurOnSubmit={true}
+                    />
+                  </View>
+                  <View style={styles.SectionStyle}>
+                    <TextInput
+                      style={styles.inputStyle}
+                      onChangeText={(UserPassword) =>
+                        setUserPassword(UserPassword)
+                      }
+                      placeholder="password" //12345
+                      placeholderTextColor="#8b9cb5"
+                      keyboardType="default"
+                      ref={passwordInputRef}
+                      onSubmitEditing={Keyboard.dismiss}
+                      blurOnSubmit={false}
+                      secureTextEntry={true}
+                      underlineColorAndroid="#f000"
+                      returnKeyType="next"
+                    />
+                  </View>
+                  {errortext != '' ? (
+                    <Text style={styles.errorTextStyle}>
+                      {errortext}
+                    </Text>
+                  ) : null}
+      
+                  <TouchableOpacity
+                    style={styles.buttonStyle}
+                    activeOpacity={0.5}
+                    onPress={() => authPostRequest()}>
+                    <Text style={styles.buttonTextStyle}>login</Text>
+                  </TouchableOpacity>
+      
+                  <Text
+                    style={styles.registerTextStyle}
+                    onPress={() => navigation.navigate('SignUp')}>
+                    need an account? sign up!
+                  </Text>
+                </KeyboardAvoidingView>
               </View>
-              <View style={styles.SectionStyle}>
-                <TextInput
-                  style={styles.inputStyle}
-                  onChangeText={(UserPassword) =>
-                    setUserPassword(UserPassword)
-                  }
-                  placeholder="password" //12345
-                  placeholderTextColor="#8b9cb5"
-                  keyboardType="default"
-                  ref={passwordInputRef}
-                  onSubmitEditing={Keyboard.dismiss}
-                  blurOnSubmit={false}
-                  secureTextEntry={true}
-                  underlineColorAndroid="#f000"
-                  returnKeyType="next"
-                />
-              </View>
-              {errortext != '' ? (
-                <Text style={styles.errorTextStyle}>
-                  {errortext}
-                </Text>
-              ) : null}
-  
-              <TouchableOpacity
-                style={styles.buttonStyle}
-                activeOpacity={0.5}
-                onPress={() => navigation.navigate('drawer')}>
-                {/* onPress={handleSubmitPress}> */}
-                <Text style={styles.buttonTextStyle}>login</Text>
-              </TouchableOpacity>
-  
-              <Text
-                style={styles.registerTextStyle}
-                onPress={() => navigation.navigate('SignUp')}>
-                need an account? sign up!
-              </Text>
-            </KeyboardAvoidingView>
+            </ScrollView>
+            </SafeAreaView>
           </View>
-        </ScrollView>
-        </SafeAreaView>
-      </View>
-    );
-  };
+        );
+      };
+  
   export default LoginScreen;
   
   const styles = StyleSheet.create({
